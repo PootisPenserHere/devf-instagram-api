ae44ebdb1c25d1bfe714a9bb35084e28df5bb127
Jose Pablo Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-12-10 20:23:35 +0000

    [mod] Sing up usses the same issue token method as login

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,12 +1,14 @@
 const Users =  require('../schemas/Users');
-const { signNewToken } = require("../services/jwt");
 const { signin } = require("../models/auth");
 
 async function signup(_,args,context,info){
     return Users.create(args.data).then(async (user) => {
-
-        let payload = {email: user.email};
-        let token = await signNewToken(payload);
+        /*
+        Since the user has already been created we can re use the login method
+        and directly query the newly created user to issue their token alongside
+        their sign up process
+         */
+        let token = await signin(user.email,user.password);
         return {token}
 
     }).catch((err) => {
